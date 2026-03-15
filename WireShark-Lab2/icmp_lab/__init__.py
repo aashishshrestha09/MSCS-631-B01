173bc430ea35e5241425828eb9cf308c99c6e3fe
Aashish Shrestha <aashishshrestha7035@gmail.com>
2026-03-14 22:04:06 -0400

    Properly export save_report-md from the package

--- WireShark-Lab2/icmp_lab/__init__.py
+++ WireShark-Lab2/icmp_lab/__init__.py
@@ -4,7 +4,7 @@ from .analyzer import analyze_pcap
 from .capture import detect_capture_interface, start_background_capture
 from .models import AnalysisResults, ICMPPacketInfo
 from .network import run_ping, run_traceroute
-from .report import print_report
+from .report import print_report, save_report_md
 
 __all__ = [
     "analyze_pcap",
@@ -15,4 +15,5 @@ __all__ = [
     "run_ping",
     "run_traceroute",
     "print_report",
+    "save_report_md",
 ]
